e3d2a2099191505a80140dc68a997d7765028704
Erwin Heitzman <erwin.heitzman@gmail.com>
2019-09-24 23:16:31 +0200

    Refactor scan.ts

--- src/scan.ts
+++ src/scan.ts
@@ -1,61 +1,48 @@
 import { readdirSync, readFileSync, statSync } from 'fs';
 import { resolve } from 'path';
 
-function foo(_line: string) {
-    const len = _line.length;
-    const singleQuoteCharCode = 34;
-    const doubleQuoteCharCode = 39;
+const findings: { [key: string]: number; } = {};
 
-    let i = 0;
-    let str = '';
-    let activeSingle = false;
-    let activeDouble = false;
+function scanLine(data: string) {
+	const states = { 'single': false, 'double': false };
+    const len = data.length;
+
+	function storeFinding(type: 'single' | 'double') {
+		if (states[type] === true) {
+			if (str.length > 0) {
+				if (findings[str]) {
+					findings[str]++;
+				} else {
+					findings[str] = 1;
+				}
+			}
+			str = '';
+			states[type] = false;
+		} else {
+			states[type] = true;
+		}
+	}
+
+	let i = 0;
+	let str = '';
 
     for (; i < len; i++) {
-        if (_line.charCodeAt(i) === 34 && activeSingle === false) {
-            if (activeDouble === true) {
-                if (str.length > 0) {
-                    if (findings[str]) {
-                        findings[str]++;
-                    } else {
-                        findings[str] = 1;
-                    }
-                }
-                str = '';
-                activeDouble = false;
-                continue;
-            }
-
-            activeDouble = true;
-            continue;
+		if (data.charCodeAt(i) === 34 && states['single'] === false) {
+			storeFinding('double');
+			return;
         }
 
-        if (_line.charCodeAt(i) === 39 && activeDouble === false) {
-            if (activeSingle === true) {
-                if (str.length > 0) {
-                    if (findings[str]) {
-                        findings[str]++;
-                    } else {
-                        findings[str] = 1;
-                    }
-                }
-                str = '';
-                activeSingle = false;
-                continue;
-            }
+        if (data.charCodeAt(i) === 39 && states['double'] === false) {
+			storeFinding('single');
+			return;
+		}
 
-            activeSingle = true;
-            continue;
-        }
-
-        if (activeDouble === true || activeSingle === true) {
-            str += _line[i];
-            continue;
+		if (states['double'] === true || states['single'] === true) {
+			str += data[i];
+			return;
         }
     }
 }
-
-const findings: { [key: string]: number; } = {};
 
 export function getFiles(dirPath: string) {
     readdirSync(dirPath).forEach((filePath) => {
@@ -68,7 +55,7 @@ export function getFiles(dirPath: string) {
         const file = readFileSync(fullPath, 'utf8');
 
         file.split('\n').forEach((line) => {
-            foo(line);
+            scanLine(line);
         });
     });
 

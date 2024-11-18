ba508031fd42e3cf676589abf1c0832f83b2cbfc
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2024-11-18 01:18:05 +0100

    fix issue that got the program stuck

--- src/getFiles/getFiles.ts
+++ src/getFiles/getFiles.ts
@@ -3,14 +3,12 @@ import { extname, sep } from 'node:path';
 import { globSync } from 'glob';
 
 export function getFiles(path: string, exclusions: string[], extensions: string[]): string[] {
-	return globSync(path, { nodir: true, realpath: true, absolute: true, follow: true, matchBase: true }).filter(
-		(path) => {
-			return (
-				path &&
-				existsSync(path) &&
-				path.split(sep).every((part) => (exclusions.length ? exclusions.some((e) => part !== e) : true)) &&
-				(extensions.length ? extensions.includes(extname(path)) : true)
-			);
-		},
-	) as string[];
+	return globSync(path, { nodir: true, realpath: true, absolute: true, matchBase: true }).filter((path) => {
+		return (
+			path &&
+			existsSync(path) &&
+			path.split(sep).every((part) => (exclusions.length ? exclusions.some((e) => part !== e) : true)) &&
+			(extensions.length ? extensions.includes(extname(path)) : true)
+		);
+	}) as string[];
 }

10542a1208052d12c7004c77002b02366bb2a146
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2025-06-12 00:10:50 +0200

    add fileCount property, improve output performance, exclude mocks in package

--- src/file/file.ts
+++ src/file/file.ts
@@ -21,26 +21,25 @@ export class File {
 			/(?:("[^"\\]*(?:\\.[^"\\]*)*")|('[^'\\]*(?:\\.[^'\\]*)*'))/g,
 		);
 
-		if (matches) {
-			matches.forEach((match) => {
-				const isNotEmpty = match && match.length > 2;
-				if (isNotEmpty) {
-					this.storeMatch(match.substring(1, match.length - 1));
-				}
-			});
-		}
+		matches?.forEach((match) => {
+			const isNotEmpty = match && match.length > 2;
+			if (isNotEmpty) {
+				this.storeMatch(match.substring(1, match.length - 1));
+			}
+		});
 	}
 
 	private storeMatch(key: string): void {
 		const value = store.find(key);
 
 		if (!value) {
-			store.add(key, { key, count: 1, files: [this.path] });
+			store.add(key, { key, count: 1, fileCount: 1, files: [this.path] });
 			return;
 		}
 
 		if (!value.files.includes(this.path)) {
 			value.files.push(this.path);
+			value.fileCount++;
 		}
 		value.count++;
 	}
